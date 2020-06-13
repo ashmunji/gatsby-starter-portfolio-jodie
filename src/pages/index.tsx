37b24ebcbe6dd9862da7487eeb7d649d27d698a5
Abhi Diddigi <adiddigi@apple.com>
2020-06-13 13:05:45 -0700

    changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,19 +32,15 @@ const Area = styled(animated.div)`
   grid-template-columns: repeat(3, 1fr);
   grid-template-rows: 35vw 40vw 25vw;
   grid-template-areas:
-    'first-project about-us about-us'
     'three-projects three-projects three-projects'
-    'instagram instagram instagram';
 
   @media (max-width: ${(props) => props.theme.breakpoints[3]}) {
     grid-template-columns: repeat(4, 1fr);
     grid-template-rows: 35vw 30vw 30vw 25vw;
 
     grid-template-areas:
-      'first-project first-project about-us about-us'
       'three-projects three-projects three-projects three-projects'
       'three-projects three-projects three-projects three-projects'
-      'instagram instagram instagram instagram';
   }
 
   @media (max-width: ${(props) => props.theme.breakpoints[1]}) {
