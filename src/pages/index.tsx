be12b9adad2f6738e799b0e62e3b67124f4d8ce1
Abhi Diddigi <adiddigi@apple.com>
2020-06-13 13:08:32 -0700

    changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,15 +32,19 @@ const Area = styled(animated.div)`
   grid-template-columns: repeat(3, 1fr);
   grid-template-rows: 35vw 40vw 25vw;
   grid-template-areas:
+    'first-project about-us about-us'
     'three-projects three-projects three-projects'
+    'instagram instagram instagram';
 
   @media (max-width: ${(props) => props.theme.breakpoints[3]}) {
     grid-template-columns: repeat(4, 1fr);
     grid-template-rows: 35vw 30vw 30vw 25vw;
 
     grid-template-areas:
+      'first-project first-project about-us about-us'
       'three-projects three-projects three-projects three-projects'
       'three-projects three-projects three-projects three-projects'
+      'instagram instagram instagram instagram';
   }
 
   @media (max-width: ${(props) => props.theme.breakpoints[1]}) {
@@ -48,9 +52,11 @@ const Area = styled(animated.div)`
     grid-template-rows: repeat(5, 38vw);
 
     grid-template-areas:
+      'first-project about-us'
       'three-projects three-projects'
       'three-projects three-projects'
       'three-projects three-projects'
+      'instagram instagram';
   }
 
   @media (max-width: ${(props) => props.theme.breakpoints[0]}) {
@@ -58,9 +64,12 @@ const Area = styled(animated.div)`
     grid-template-rows: repeat(6, 50vw);
 
     grid-template-areas:
+      'first-project'
+      'about-us'
       'three-projects'
       'three-projects'
       'three-projects'
+      'instagram';
   }
 `
 
