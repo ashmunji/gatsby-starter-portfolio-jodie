cbd6544cf45a251ebd7a51697aefbe4d745f9dd8
Abhi Diddigi <adiddigi@apple.com>
2020-06-13 11:55:18 -0700

    changes

--- config/index.js
+++ config/index.js
@@ -1,21 +1,21 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Jodie', // Navigation and Site Title
-  siteTitleAlt: 'Jodie - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteTitleShort: 'Jodie', // short_name for manifest
-  siteHeadline: 'Come & Enjoy our excellent photos', // Headline for schema.org JSONLD
-  siteUrl: 'https://jodie.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitle: 'Space Narrative', // Navigation and Site Title
+  siteTitleAlt: 'Space Narrative - A Landscape and Interior Design company.', // Alternative Site title for SEO
+  siteTitleShort: 'SpaceNarrative', // short_name for manifest
+  siteHeadline: '... hello world', // Headline for schema.org JSONLD
+  siteUrl: 'https://spacenarrarive.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo.png', // Used for SEO and manifest
-  siteDescription: 'Image-heavy photography portfolio with colorful accents & great typography',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  siteDescription: '.... hello world2',
+  author: 'AshwiniMunji', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@jodie', // Twitter Username
-  ogSiteName: 'jodie', // Facebook Site Name
+  //userTwitter: '@jodie', // Twitter Username
+  //ogSiteName: 'jodie', // Facebook Site Name
   ogLanguage: 'en_US', // og:language
-  googleAnalyticsID: 'UA-XXXXXX-X',
+  //googleAnalyticsID: 'UA-XXXXXX-X',
 
   // Manifest and Progress color
   themeColor: '#db7436',
